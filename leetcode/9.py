7dc238eed9d69d911772160e59e3993d6ddbd049
Dowon Jang <rosewoodowon@gmail.com>
2022-02-08 19:16:13 +0900

    test

--- leetcode/9.py
+++ leetcode/9.py
@@ -1,4 +1,8 @@
+import time
+
 import self as self
+
+start_time = time.time()
 
 
 class Solution:
@@ -11,3 +15,7 @@ class Solution:
 
 x = -121
 print(Solution.isPalindrome(self, x))
+
+
+end_time = time.time()
+print("time :", end_time - start_time)
