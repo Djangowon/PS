c25678a7caaec4fc6a69c4344dda00ee05f0ff0c
Dowon Jang <rosewoodowon@gmail.com>
2022-02-21 20:52:03 +0900

    12934

--- programmers/12934.py
+++ programmers/12934.py
@@ -8,7 +8,7 @@ n = int(input())
 
 
 def solution(n):
-    return pow(math.sqrt(n)+1, 2) if int(math.sqrt(n)) == math.sqrt(n) else '-1'
+    return pow(math.sqrt(n)+1, 2) if int(math.sqrt(n)) == math.sqrt(n) else -1
 
 
 print(solution(n))
