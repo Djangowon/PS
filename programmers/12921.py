f0e70103fa27923e83012144971c78de493f6856
Dowon Jang <rosewoodowon@gmail.com>
2022-04-08 02:41:05 +0900

    12921

--- programmers/12921.py
+++ programmers/12921.py
@@ -2,15 +2,15 @@ n = int(input())
 
 
 def solution(n):
-    sieve = [True] * n
+    sieve = [True] * (n + 1)
 
     m = int(n ** 0.5)
     for i in range(2, m + 1):
         if sieve[i] == True:
-            for j in range(i+i, n, i):
+            for j in range(i+i, n + 1, i):
                 sieve[j] = False
 
-    return [i for i in range(2, n) if sieve[i] == True]
+    return [i for i in range(2, n + 1) if sieve[i] == True]
 
 
 print(solution(n))
