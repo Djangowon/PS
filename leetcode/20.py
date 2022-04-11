57b75a9f90ae5563f2e9b0c62d0065a521747c5f
Dowon Jang <rosewoodowon@gmail.com>
2022-04-11 22:41:57 +0900

    20

--- leetcode/20.py
+++ leetcode/20.py
@@ -1,6 +1,25 @@
+from self import self
+
 s = str(input())
 
 
 class Solution:
     def isValid(self, s: str) -> bool:
+        brackets = {'}':'{',')':'(',']':'['}
+        stack = []
+        for i in s:
+            if i in brackets.values():
+                stack.append(i)
+            else:
+                if stack and brackets[i] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
 
+        if stack:
+            return False
+
+        return True
+
+
+print(Solution.isValid(self, s))
