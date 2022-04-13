fa1097896741a83eda096d761a656af7c7d570b9
Dowon Jang <rosewoodowon@gmail.com>
2022-04-13 20:27:58 +0900

    15552

--- baekjoon/15552.py
+++ baekjoon/15552.py
@@ -1,5 +1,7 @@
-T = int(input())
+import sys
+
+T = int(sys.stdin.readline())
 
 for _ in range(T):
-    A, B = (list(map(int, input().split())))
+    A, B = list(map(int, sys.stdin.readline().split()))
     print(A + B)
