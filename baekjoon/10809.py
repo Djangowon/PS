1577070821df7482624a90dfcbbfc58b6d26a060
Dowon Jang <rosewoodowon@gmail.com>
2022-04-17 00:07:00 +0900

    10809

---
+++ baekjoon/10809.py
@@ -0,0 +1,11 @@
+import sys
+
+S = str(sys.stdin.readline())
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+for i in alphabet:
+    print(S.find(i), end=' ')
+
