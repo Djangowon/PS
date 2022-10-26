8d78fd2b5482255c370a2598121061aaf8c498bc
Dowon Jang <rosewoodowon@gmail.com>
2022-10-26 23:53:41 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        int idx = 0;
+        for (String str : strlist) {
+            answer[idx] = str.length();
+            idx++;
+        }
+        return answer;
+    }
+}
