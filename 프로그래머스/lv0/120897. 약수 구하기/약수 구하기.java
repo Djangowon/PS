9074e16e11c0150f2777adf6a6acdb127ad142eb
Dowon Jang <rosewoodowon@gmail.com>
2022-11-06 23:55:43 +0900

    [level 0] Title: 약수 구하기, Time: 2.12 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -1,21 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int n) {
-        int[] arr = new int[10000];
-        int idx =0;
-       
-        for(int i = 1; i<=n; i++) {
-			if(n % i ==0) {
-				arr[idx] = i;
-				idx++;
-			}
-		}
-        
-		int[] answer = new int [idx];
-		for(int i = 0; i < idx; i++) {
-			if(arr[i] != 0) {
-				answer[i] = arr[i];
-			}
-		}
-        return answer;
+        List<Integer> answer = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(n % i == 0){
+                answer.add(i);
+            }
+        }
+        return answer.stream().mapToInt(x -> x).toArray();
     }
 }
