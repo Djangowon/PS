d22cbb1fb12f16b43b07cb2572957ce9c3f3625c
Dowon Jang <rosewoodowon@gmail.com>
2022-12-01 17:11:27 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        while (true) {
+            if ((answer * 6) % n == 0) {
+                break;
+            } else {
+                answer += 1;
+            }
+        }
+        
+        return answer;
+    }
+}
