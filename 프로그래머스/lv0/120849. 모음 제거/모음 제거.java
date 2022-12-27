c70faec4de6d81eceef295249b96e187d10b82c1
Dowon Jang <rosewoodowon@gmail.com>
2022-12-27 19:47:15 +0900

    [level 0] Title: 모음 제거, Time: 0.11 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.replace("a", "").replace("e", "").replace("i", "").replace("o", "").replace("u", "");
+        String answer = my_string.replaceAll("[aeiou]", "");
         
         return answer;
     }
